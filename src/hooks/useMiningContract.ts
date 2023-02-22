652e4027c4cc6c9d63befdaf0a1f2fc30d3b47af
Vikanong <1373576877@qq.com>
2023-02-22 18:35:55 +0800

    update

--- src/hooks/useMiningContract.ts
+++ src/hooks/useMiningContract.ts
@@ -5,47 +5,56 @@ import MiningAbi from "../constants/abi/Mining.json"
 import contractsAddress from 'constants/contractsAddress'
 import { getContractAddress } from '../utils/contractUtils'
 
-export const usePoolsIds = () => {
-    const [ids, setIds] = useState<any[]>([]);
-    const contract = useMiningContract();
-    const getIds = async () => {
+import { Contract } from '@ethersproject/contracts'
+
+// export const usePoolsIds = () => {
+//     const [ids, setIds] = useState<any[]>([]);
+//     const contract = useMiningContract();
+//     const getIds = async () => {
+//         const length = await contract?.poolsLength();
+//         const list = [...new Array(length.toNumber()).keys()];
+//         setIds(list);
+//     }
+//     useEffect(() => {
+//         if (contract) getIds()
+//     }, [contract])
+//     return ids
+// }
+
+// export const usePools = (ids: number[]) => {
+
+//     console.log(ids);
+//     const [pools, setPools] = useState([]);
+//     const contract = useMiningContract();
+//     const address = getContractAddress(contractsAddress.mining);
+//     const stateCalls = ids.map((i) => ({
+//         address: address,
+//         name: 'state',
+//         params: [i],
+//     }))
+//     // const getPools = async () => {
+//     //     const stateCalls = ids.map((i) => ({
+//     //         address: address,
+//     //         name: 'state',
+//     //         params: [i],
+//     //     }))
+//     //     const state = await useMulticall(MiningAbi, stateCalls);
+//     // }
+//     useEffect(() => {
+//         if (contract) {
+//             // getPools()
+//         }
+//     }, [contract])
+//     return pools
+// }
+
+export default function useMining(contract: Contract) {
+    const pools = async () => {
         const length = await contract?.poolsLength();
-        const list = [...new Array(length.toNumber()).keys()];
-        setIds(list);
     }
-    useEffect(() => {
-        if (contract) getIds()
-    }, [contract])
-    return ids
-}
 
-export const usePools = (ids: number[]) => {
 
-    console.log(ids);
-    const [pools, setPools] = useState([]);
-    const contract = useMiningContract();
-    const address = getContractAddress(contractsAddress.mining);
-    const stateCalls = ids.map((i) => ({
-        address: address,
-        name: 'state',
-        params: [i],
-    }))
-    // const data = useMulticall(MiningAbi, stateCalls);
-
-    // setPools(data)
-
-    // const getPools = async () => {
-    //     const stateCalls = ids.map((i) => ({
-    //         address: address,
-    //         name: 'state',
-    //         params: [i],
-    //     }))
-    //     const state = await useMulticall(MiningAbi, stateCalls);
-    // }
-    useEffect(() => {
-        if (contract) {
-            // getPools()
-        }
-    }, [contract])
-    return pools
+    return {
+        pools
+    }
 }
--- src/views/mining/index.tsx
+++ src/views/mining/index.tsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react"
-import ConnectWalletModal from "../../components/ConnectWalletModal"
+import ConnectWalletModal from "@/components/ConnectWalletModal"
 import { useActiveWeb3React } from 'hooks/web3hooks'
-import { usePoolsIds, usePools } from 'hooks/useMiningContract'
+// import { usePoolsIds, usePools } from 'hooks/useMiningContract'
+
+import { useMiningContract } from 'hooks/useContract'
+import { getPools } from "@/utils/miningHelpers"
 
 import "./index.less"
 
 const Mining: React.FC = () => {
-
+  const [pools, setPools] = useState<any[]>([]);
   const { account } = useActiveWeb3React();
-  const ids = usePoolsIds();
-  const pools = usePools(ids);
+  const miningContract = useMiningContract();
+  // const ids = usePoolsIds();
+  // const pools = usePools(ids);
+
+  useEffect(() => {
+    const get = async () => {
+      const list = await getPools(miningContract);
+    }
+    get()
+  }, [miningContract])
 
 
 
